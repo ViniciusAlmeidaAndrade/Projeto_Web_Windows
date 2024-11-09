3e56dc7f844e91a36fd693c8959ca036dce80997
MatheusMontP <matheusmontenegrofacul@gmail.com>
2024-11-09 19:41:28 -0300

    att

--- Projeto_Web/projeto_web/roles.py
+++
@@ -1,8 +0,0 @@
-from rolepermissions.roles import AbstractUserRole
-from django.contrib.auth.models import User
-
-class Gerente(AbstractUserRole):
-    available_permissions = {'editar_estoque': True}
-
-class Tecnico(AbstractUserRole):
-    available_permissions = {'ver_estoque': True}
--- Projeto_Web/projeto_web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls import handler404
-from login import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('login.urls'))
-]
